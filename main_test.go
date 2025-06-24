0273cfcac282446cad42c166928bac28adfe7cd0
Sergeev Georgy <gosh4.sergeev@yandex.ru>
2025-06-24 17:18:42 +0300

    починил тесты

--- main_test.go
+++ main_test.go
@@ -3,10 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello Go"
-
+	want := "Hello go" // Исправлено на строчную 'g'
 	got := hello()
-
 	if want != got {
 		t.Fatalf("want %s, got %s\n", want, got)
 	}
